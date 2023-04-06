9d11c945601c1c0fff88a5c5b5445cf039b6bd99
anton-sosonskyi <anton.sosonskyi@gmail.com>
2023-04-06 18:48:16 +0300

    fixed fonts-face

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom';
+import React from 'react';
 import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 import { CatalogProvider } from './context';
-import './index.scss';
+import './styles/index.scss';
 
 ReactDOM.render(
   <HashRouter>
